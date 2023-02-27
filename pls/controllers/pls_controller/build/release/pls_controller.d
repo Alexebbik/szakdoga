139fdf09dd0759f806b453f7e794fc31a8b73c9a
Alexebbik <112709677+Alexebbik@users.noreply.github.com>
2023-02-27 15:33:58 +0100

    Delete pls directory

--- pls/controllers/pls_controller/build/release/pls_controller.d
+++
@@ -1,8 +0,0 @@
-build/release/pls_controller.o: pls_controller.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/gps.h \
- C:\Program\ Files\Webots/include/controller/c/webots/compass.h
