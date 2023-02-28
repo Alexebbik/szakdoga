896c7289c470f402203b3e526fd3c2f7b36db174
Alexebbik <112709677+Alexebbik@users.noreply.github.com>
2023-02-28 14:22:34 +0100

    Add files via upload

---
+++ szakdolgozat_webots/controllers/mainrobot_controller/mainrobot_controller.py
@@ -0,0 +1,72 @@
+import math
+from controller import *
+from vehicle import *
+
+
+driver = Driver()
+driver.setSteeringAngle(0)
+#car.setRightSteeringAngle(0)
+#car.setLeftSteeringAngle(0)
+#driver.setCruisingSpeed(20)
+TIME_STEP = 64
+ds_list = ["dsRearLeft","dsLeft","dsFrontLeft","dsFrontRight","dsRight","dsRearRight"]
+sensor_value = ['','','','','','']
+    
+    
+    
+#currentTime = int(driver.getCurrentTime())
+
+
+'''def __init__(driver):
+    wb.wb_driver_init()'''
+    
+def run():
+
+    
+    '''# Initialize the distance sensor
+    dsL = DistanceSensor("dsLeft")
+    dsL.enable(TIME_STEP)
+    dsR = DistanceSensor("dsRight")
+    dsR.enable(TIME_STEP)'''
+    
+    driver.setBrakeIntensity(0.0)
+    
+    while driver.step() != -1:
+        
+        '''1for i in range(len(ds_list)):
+            sensor_value[i] = ds_list[i].getValue()'''
+            
+        print("dsRight:",sensor_value[4])
+        #sensorR_value = dsRight.getValue()
+        #print("dsrightvalue:",sensorR_value)
+        '''sensorL_value = dsL.getValue()
+        print("dsleft:",sensorL_value)
+        sensorR_value = dsR.getValue()
+        print("dsright:",sensorR_value)'''
+        
+        #get the north vector from the compass
+        #north = compass.getValues()
+        #calculate the orientation of the robot
+        #orientation = math.atan2(north[0], north[2])
+        #print the orientation
+        #print("Orientation:", orientation)
+        
+        #GPSvalues = gps.getValues()
+        #print("GPSvalues:",GPSvalues)
+        
+        
+        #driver.setSteeringAngle(0.5)
+        driver.setCruisingSpeed(1)
+        #print("currentspeed:",driver.getCurrentSpeed())
+        #print("cruisingspeed:",driver.getTargetCruisingSpeed())
+        #print("rightangle:",car.getRightSteeringAngle())
+        #print("leftangle:",car.getLeftSteeringAngle())
+        print("asd")
+'''def __del__(driver):
+    wb.wb_driver_cleanup()'''
+    
+if __name__ == "__main__":
+    run()
+    
+    
+    
---
+++ szakdolgozat_webots/controllers/my_controller/my_controller.py
@@ -0,0 +1,35 @@
+"""my_controller controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from vehicle import Driver
+
+
+# create the Robot instance.
+robot = Driver()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getDevice('motorname')
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step() != -1:
+    robot.setCruisingSpeed(10)
+    robot.setSteeringAngle(0)
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    #  val = ds.getValue()
+
+    # Process sensor data here.
+
+    # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    pass
+
+# Enter here exit cleanup code.
